905dfae939d6c1e07b1f500555c97903cf3775b0
MailzMan <120980866+MailzMan@users.noreply.github.com>
2022-12-20 17:44:47 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+$('.single-item').slick({
+    infinite: true,
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+      $(".zoom-me img").css({
+          width: (100 + scroll/5)  + "%",
+          top: -(scroll/10)  + "%",
+          //Blur suggestion from @janwagner: https://codepen.io/janwagner/ in comments
+          "-webkit-filter": "blur(" + (scroll/200) + "px)",
+          filter: "blur(" + (scroll/200) + "px)"
+      });
+  });
+
+  $(document).ready(function() {
+	$('.menu-burger__header').click(function(){
+        $('.menu-burger__header').toggleClass('open-menu');
+        $('.header__nav').toggleClass('open-menu');
+        $('body').toggleClass('fixed-page');
+	});
+});
+
+let popupBg = document.querySelector('.popup__bg'); // Фон попап окна
+let popup = document.querySelector('.popup'); // Само окно
+let openPopupButtons = document.querySelectorAll('.open-popup'); // Кнопки для показа окна
+let closePopupButton = document.querySelector('.close-popup'); // Кнопка для скрытия окна
+
+openPopupButtons.forEach((button) => { // Перебираем все кнопки
+  button.addEventListener('click', (e) => { // Для каждой вешаем обработчик событий на клик
+      e.preventDefault(); // Предотвращаем дефолтное поведение браузера
+      popupBg.classList.add('active'); // Добавляем класс 'active' для фона
+      popup.classList.add('active'); // И для самого окна
+  })
+});
+
+closePopupButton.addEventListener('click',() => { // Вешаем обработчик на крестик
+  popupBg.classList.remove('active'); // Убираем активный класс с фона
+  popup.classList.remove('active'); // И с окна
+});
+
+document.addEventListener('click', (e) => { // Вешаем обработчик на весь документ
+  if(e.target === popupBg) { // Если цель клика - фот, то:
+      popupBg.classList.remove('active'); // Убираем активный класс с фона
+      popup.classList.remove('active'); // И с окна
+  }
+});
+
+
+
